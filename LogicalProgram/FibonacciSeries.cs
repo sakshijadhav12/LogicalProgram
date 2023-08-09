beaa7992079bb70a826235a15466f0f82d9ae05e
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-10 01:32:02 +0530

    Fibonacci series program

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LogicalProgram;
+
+public static class FabonacciSeries
+{
+    public static void Solve()
+    {
+        Console.WriteLine("Enter the number of terms for Fibonacci series:");
+        int numTerms = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci series:");
+        GenerateFibonacciSeries(numTerms);
+    }
+
+    private static void GenerateFibonacciSeries(int numTerms)
+    {
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        for (int i = 0; i < numTerms; i++)
+        {
+            Console.Write($"{firstTerm} ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+
+        Console.WriteLine();
+    }
+}
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LogicalProgram
+﻿using System;
+
+namespace LogicalProgram;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Logical Programs");
+        Console.WriteLine("select Option \n1.FibonacciSeries");
+       int options=Convert.ToInt32(Console.ReadLine());
+        switch (options)
         {
-            Console.WriteLine("Logical Programs");
+            case 1:
+                FabonacciSeries.SolveFabonacciSeries();
+                break;
+
         }
-    }
+
+
+        }
 }
